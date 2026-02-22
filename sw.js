b94a926df7769720e1baea8d3deefcd0f7425032
weytom-coder <weytom@gmail.com>
2026-02-22 12:01:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE='tf-app-v6';
+const CACHE='tf-app-v6_1';
 const SHELL=['./','./index.html','./style.css','./app.js','./training.html','./training_mo.html','./training_do.html','./training_sa.html','./details.html','./settings.html','./ernaehrung.html','./progress.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
